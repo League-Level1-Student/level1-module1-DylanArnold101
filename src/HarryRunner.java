21dc53f5851c7290c74bd0b49d0d683d4bb4e5ae
DylanArnold101 <league@WTS10.attlocal.net>
2018-08-19 11:27:15 -0700

    Harry runner/RaceCarRunner(Ip)
    fun

--- src/HarryRunner.java
+++ src/HarryRunner.java
@@ -1,4 +1,11 @@
 
 public class HarryRunner {
+public static void main(String[] args) {
+	HarryPotter Harry = new HarryPotter();
+	Harry.makeInvisible(true);
+	Harry.spyOnSnape();
+	Harry.makeInvisible(false);
+	Harry.castSpell("stupefy");
 
+}
 }
